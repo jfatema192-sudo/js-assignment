c94d30778a997eaa7e3da591d746a71d67664d90
jfatema192-sudo <jfatema192@gmail.com>
2026-03-30 12:46:26 +0900

    Add scoring and repetition logic in assigment.js

---
+++ assigment.js
@@ -0,0 +1,13 @@
+let myScore = 85;
+
+if (myScore > 90) {
+    console.log("A");
+} else if (myScore >= 80 && myScore <= 90) {
+    console.log("B");
+} else {
+    console.log("C");
+}
+
+for (let i = 1; i <= 10; i++) {
+    console.log("2".repeat(i));
+}
